9e95ca1d10c91c0b32cfe7ed87bca2404907b3d5
Andrew <andrew.suponev@gmail.com>
2024-01-23 22:37:59 +0400

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const content = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+
+  try {
+    await fs.readFile(fileName);
+    throw new Error(errorMessage);
+  } catch (error) {
+    await fs.writeFile(fileName, content);
+    if (error.message === errorMessage) {
+      console.log(error.message);
+    }
+  }
 };
 
 await create();
