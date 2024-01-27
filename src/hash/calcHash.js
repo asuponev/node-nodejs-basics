8b1e4b1788a734c115b67d4ab61db96172504836
Andrew <andrew.suponev@gmail.com>
2024-01-27 20:49:17 +0400

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filesFolder = path.join(__dirname, 'files');
+  const fileName = 'fileToCalculateHashFor.txt';
+  const filePath = path.join(filesFolder, fileName);
+
+  const stream = fs.createReadStream(filePath);
+  const hash = crypto.createHash('sha256');
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const sha256Hash = hash.digest('hex');
+    console.log(`SHA256 hash for ${fileName}: ${sha256Hash}`);
+  });
+
+  stream.on('error', (error) => {
+    console.error(error.message);
+  });
 };
 
 await calculateHash();
