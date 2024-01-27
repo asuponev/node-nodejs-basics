1d08093825cb639efc1757d2c366c3d3c2b72073
Andrew <andrew.suponev@gmail.com>
2024-01-27 20:23:28 +0400

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  for (let i = 2; i < args.length; i += 2) {
+    const name = args[i].startsWith('--') ? args[i].slice(2) : args[i];
+    const value = args[i + 1];
+    console.log(`${name} is ${value}`);
+  }
 };
 
 parseArgs();
