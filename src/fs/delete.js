3cbdb24b947cb70351f83dd94b1549f8b8c2aa0f
Andrew <andrew.suponev@gmail.com>
2024-01-27 19:59:26 +0400

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filesFolder = path.join(__dirname, 'files');
+  const fileName = 'fileToRemove.txt';
+  const filePath = path.join(filesFolder, fileName);
+  const errorMessage = 'FS operation failed';
+
+  try {
+    try {
+      await fs.stat(filePath);
+    } catch (error) {
+      throw new Error(errorMessage);
+    }
+
+    await fs.unlink(filePath);
+  } catch (error) {
+    if (error.message === errorMessage) {
+      console.log(error.message);
+    }
+  }
 };
 
 await remove();
