281c46f294c8971d2b05242a29c73176cf866412
Andrew <andrew.suponev@gmail.com>
2024-01-29 21:36:32 +0400

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filesFolder = path.join(__dirname, 'files');
+  const fileName = 'fileToRead.txt';
+  const filePath = path.join(filesFolder, fileName);
+  const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  stream.pipe(process.stdout);
+
+  stream.on('error', (error) => {
+    console.error(error.message);
+  });
 };
 
 await read();
