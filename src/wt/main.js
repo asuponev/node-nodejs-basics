4c22d2d56d8e165f65bfc1d406fb1cd18d162d7f
Andrew <andrew.suponev@gmail.com>
2024-01-29 23:32:42 +0400

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker, isMainThread } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpu = os.cpus().length;
+
+  if (isMainThread) {
+    console.log(`${cpu} worker threads`);
+    const result = [];
+
+    const handleMessage = (message, index) => {
+      if (message.status === 'resolved') {
+        result[index] = { status: 'resolved', data: message.data };
+      } else {
+        result[index] = { status: 'error', data: null };
+      }
+
+      if (result.length === cpu) {
+        console.log(`${JSON.stringify(result)}`);
+      }
+    };
+
+    for (let i = 0; i < cpu; i++) {
+      const worker = new Worker('./src/wt/worker.js', { workerData: 10 + i });
+      worker.on('message', (message) => handleMessage(message, i));
+    }
+  }
 };
 
 await performCalculations();
