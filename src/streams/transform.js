cc142318bf93fcaf82d379e6c587216d133f2f26
Andrew <andrew.suponev@gmail.com>
2024-01-29 22:08:32 +0400

    feat: implement streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const stream = new Transform({
+    transform(data, encoding, callback) {
+      const result = data.toString().split('').reverse().join('');
+      callback(null, result);
+      console.log('\n\nEnter something and press Ctrl+C to finish')
+    },
+  });
+
+  console.log('Enter something and press Ctrl+C to finish')
+
+  process.stdin.pipe(stream).pipe(process.stdout);
+
+  stream.on('error', (error) => {
+    console.error(error.message);
+  });
 };
 
 await transform();
